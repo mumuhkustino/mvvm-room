f89d69fc792f842b4bc0adc5c3e30c66e7d88f9e
Mumuh Andev <mumuhandev@gmail.com>
2021-06-14 07:58:27 +0700

    Add Time For DB, View, and All in Update fragment.

--- app/src/main/java/com/example/arsitektur_mvvm_and_room/ui/crud/update/UpdateViewModel.java
+++ app/src/main/java/com/example/arsitektur_mvvm_and_room/ui/crud/update/UpdateViewModel.java
@@ -11,24 +11,34 @@ import com.example.arsitektur_mvvm_and_room.utils.rx.SchedulerProvider;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 import io.reactivex.Flowable;
 public class UpdateViewModel extends BaseViewModel<UpdateNavigator> {
     private final MutableLiveData<Long> numOfRecord;
 
-    private final MutableLiveData<Long> executionTime;
+    private final MutableLiveData<Long> databaseUpdateTime;
+
+    private final MutableLiveData<Long> allUpdateTime;
+
+    private final MutableLiveData<Long> viewUpdateTime;
 
     private MutableLiveData<List<Medical>> medicalListLiveData;
 
     public UpdateViewModel(DataManager dataManager, SchedulerProvider schedulerProvider) {
         super(dataManager, schedulerProvider);
         this.numOfRecord = new MutableLiveData<>();
-        this.executionTime = new MutableLiveData<>();
+        this.databaseUpdateTime = new MutableLiveData<>();
+        this.allUpdateTime = new MutableLiveData<>();
+        this.viewUpdateTime = new MutableLiveData<>();
         this.medicalListLiveData = new MutableLiveData<>();
     }
 
     public void updateDatabase(Long numOfData) {
-        long startTime = System.currentTimeMillis();
+        AtomicLong viewUpdateTime = new AtomicLong(0);
+        AtomicLong updateDbTime = new AtomicLong(0);
+        AtomicLong updateTime = new AtomicLong(0);
+        AtomicLong allUpdateTime = new AtomicLong(System.currentTimeMillis());
         AtomicInteger index = new AtomicInteger(0);
         List<Medical> medicals = new ArrayList<>();
         getCompositeDisposable().add(getDataManager()
@@ -52,14 +62,24 @@ public class UpdateViewModel extends BaseViewModel<UpdateNavigator> {
                     }
                     return Flowable.fromIterable(medicineList);
                 })
-                .concatMap(medicine -> getDataManager().updateDatabaseMedicine(medicine))
+                .concatMap(medicine -> {
+                    updateTime.set(System.currentTimeMillis());
+                    return getDataManager().updateDatabaseMedicine(medicine);
+                })
+                .doOnNext(aBoolean -> {
+                    if (aBoolean)
+                        updateDbTime.set(updateDbTime.get() + (System.currentTimeMillis() - updateTime.get()));
+                })
                 .observeOn(getSchedulerProvider().ui())
                 .subscribe(aBoolean -> {
                             if (index.get() == numOfData) {
                                 this.numOfRecord.setValue(index.longValue()); //Change number of record
-                                long endTime = System.currentTimeMillis();
-                                long timeElapsed = endTime - startTime; //In MilliSeconds
-                                this.executionTime.setValue(timeElapsed); //Change execution time
+                                this.databaseUpdateTime.setValue(updateDbTime.get()); //Change execution time
+                                AtomicLong endTime = new AtomicLong(System.currentTimeMillis());
+                                AtomicLong timeElapsed = new AtomicLong(endTime.get() - allUpdateTime.get());
+                                viewUpdateTime.set(timeElapsed.get() - updateDbTime.get());
+                                this.viewUpdateTime.setValue(viewUpdateTime.get());
+                                this.allUpdateTime.setValue(timeElapsed.get());
                                 Log.d("UVM", "updateDatabase: " + index.get());
                                 index.getAndIncrement();
                             }
@@ -80,8 +100,16 @@ public class UpdateViewModel extends BaseViewModel<UpdateNavigator> {
         return numOfRecord;
     }
 
-    public LiveData<Long> getExecutionTime() {
-        return executionTime;
+    public LiveData<Long> getDatabaseUpdateTime() {
+        return databaseUpdateTime;
+    }
+
+    public MutableLiveData<Long> getAllUpdateTime() {
+        return allUpdateTime;
+    }
+
+    public MutableLiveData<Long> getViewUpdateTime() {
+        return viewUpdateTime;
     }
 
     public void onClick() {
